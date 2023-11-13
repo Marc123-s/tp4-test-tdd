3d8e71e7947f7037852461c7ec70081a76d1e77b
Marc Sakr <skm4139a@ensfsi.onmicrosoft.com>
2023-11-13 09:55:09 +0100

    fizzbuzz1 erreur

---
+++ src/tp4testtdd/fizz.java
@@ -0,0 +1,10 @@
+package tp4testtdd;
+
+public class fizz {
+
+	public static Object fizzbuzz(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ test/tp4testtdd/fizztest.java
@@ -0,0 +1,13 @@
+package tp4testtdd;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class fizztest {
+@Test
+	void test() {
+		assertEquals(fizz.fizzbuzz(1), "1");
+		
+	}
+}
